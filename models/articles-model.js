d90021943da7f7eec799b6a4be562aedb62b38ba
taslimy <taslimer@gmail.com>
2019-06-24 17:04:48 -0400

    Removed an endpoint &  added a readme with endpoints & database design

--- models/articles-model.js
+++ models/articles-model.js
@@ -3,7 +3,7 @@ const db = require("../data/dbConfig");
 module.exports = {
   find,
   findById,
-  getArticleList,
+  //getArticleList,
   insert,
   update,
   remove
@@ -17,9 +17,9 @@ function findById(id) {
   return db("articles").where({ id });
 }
 
-function getArticleList(user_id) {
-  return db("articles").where({ user_id });
-}
+// function getArticleList(user_id) {
+//   return db("articles").where({ user_id });
+// }
 
 function insert(article) {
   return db("articles").insert(article);
