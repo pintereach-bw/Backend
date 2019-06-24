a1cb7581b5f86017348a07b9e2543f115f2385ee
taslimy <taslimer@gmail.com>
2019-06-24 13:45:05 -0400

    Fixing issues to deploy on heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.use(express());
 server.use(express.json());
 server.use(helmet());
 server.use(cors(corsOptions));
-server.use(logger());
+// server.use(logger());
 
 server.get("/", (req, res) => {
   res.send(`<h1>THE SERVER IS LIVE!</h1>`);
@@ -25,9 +25,9 @@ server.use("/api/auth", require("./auth/auth-router")); // REGISTRATION & LOGIN
 server.use("/api/articles", require("./routes/articles-route")); // Article Route
 
 // # Logger # //
-function logger(req, res, next) {
-  console.log(` [${new Date().toISOString()}] ${req.method} to ${req.url}`);
-  next();
-}
+// function logger(req, res, next) {
+//   console.log(` [${new Date().toISOString()}] ${req.method} to ${req.url}`);
+//   next();
+// }
 
 module.exports = server;
