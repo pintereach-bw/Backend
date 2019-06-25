49d0444b68c74d4ed2c5c3badab7b90eb68e09be
taslimy <taslimer@gmail.com>
2019-06-25 17:11:14 -0400

    Added some tests for the article router.

--- routes/articles-route.js
+++ routes/articles-route.js
@@ -9,13 +9,12 @@ const restricted = require("../middlewares/restricted-middleware");
 router.get("/", restricted, async (req, res) => {
   try {
     const articles = await Articles.find();
-    res
-      .status(200)
-      .json({ articles, message: "You have gotten articles back" });
-  } catch (error) {
+    res.status(200).json(articles);
+    // console.log(res)
+  } catch (err) {
     res
       .status(500)
-      .json({ success: false, message: "Unable to find Articles" });
+      .json({ err, message: "Unable to find Articles" });
   }
 });
 
---
+++ routes/articles-router.test.js
@@ -0,0 +1,41 @@
+const request = require("supertest");
+const db = require("../data/dbConfig");
+
+const server = require("../server");
+
+describe("article-route.js with middleware", () => {
+  afterEach(async () => {
+    await db("articles").truncate();
+  });
+  it("should set to testing env", () => {
+    expect(process.env.DB_ENV).toBe("testing");
+  });
+
+  it("should return 401 unauthorized", async () => {
+    const res = await request(server).get("/api/articles/");
+    expect(res.status).toBe(401);
+  });
+
+  it("should return 401 unauthorized for /:id", async () => {
+    const res = await request(server).get("/api/articles/:id/");
+    expect(res.status).toBe(401);
+  });
+});
+
+// describe("Should not POST '/api/articles'", () => {
+//   it("should return 200 ok", async () => {
+//     const articles = {
+//       title: "Pokemon electronic game",
+//       summary: "This is a little summart about the article..",
+//       link: "https://www.britannica.com/topic/Pokemon-electronic-game",
+//       image:
+//         "https://cdn.britannica.com/s:700x450/70/122270-004-0564DF2A.jpg",
+//       category_name: "Test Article"
+//     };
+//     const res = await request(server)
+//       .post("/games")
+//       .send(game);
+//     expect(res.status).toEqual(201);
+//   });
+
+// });
